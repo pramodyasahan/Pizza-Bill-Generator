422a6741c795bf9c66ba9971c9a16fb7915a7347
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-02-09 17:16:07 +0530

    feat: create DeluxPizza class

---
+++ src/src/DeluxPizza.java
@@ -0,0 +1,20 @@
+public class DeluxPizza extends Pizza {
+    public DeluxPizza(boolean veg) {
+        super(veg);
+    }
+
+    @Override
+    public void addExtraCheese() {
+        super.addExtraCheese();
+    }
+
+    @Override
+    public void addExtraToppings() {
+        super.addExtraToppings();
+    }
+
+    @Override
+    public void takeAway() {
+        super.takeAway();
+    }
+}
